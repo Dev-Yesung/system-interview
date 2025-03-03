d88f2f81df6c0de27fc0258deb6b1d8e907fe52b
Dev-Yesung <yeasung67@gmail.com>
2025-03-03 22:14:05 +0900

    update

---
+++ rate_limitation/src/main/java/com/ysng/rate_limitation/RateLimitationApplication.java
@@ -0,0 +1,13 @@
+package com.ysng.rate_limitation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimitationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateLimitationApplication.class, args);
+	}
+
+}
